7cd230d8206e159d8f12ccff840d93e50f7ef1a9
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-01 16:14:56 -0500

    basic starter code of Front end using react initialized and also added tailwindcss

---
+++ covergenie-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-800">
+      <div className='h-16 flex items-center'>
+        <p>Employee</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
