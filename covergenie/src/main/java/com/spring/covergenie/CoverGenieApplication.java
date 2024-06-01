63f52dc03131e2c030fdbac865f9929497bc8961
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-01 15:55:06 -0500

    Spring Boot backend project initialized!

---
+++ covergenie/src/main/java/com/spring/covergenie/CoverGenieApplication.java
@@ -0,0 +1,13 @@
+package com.spring.covergenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverGenieApplication.class, args);
+	}
+
+}
---
+++ covergenie/src/test/java/com/spring/covergenie/CoverGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.covergenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
